9554525bcdf0c6eb69bb17e5af8fbe3e021e7248
KikairyuLatias <44108108+KikairyuLatias@users.noreply.github.com>
2019-02-10 14:52:18 -1000

    Updating code
    If someone wishes to help fix the thing to enable negating multiple cards, then that would be greatly appreciated :)

--- script/c90000663.lua
+++ script/c90000663.lua
@@ -3,15 +3,28 @@ function c90000663.initial_effect(c)
 	--link summon
 	aux.AddLinkProcedure(c,nil,2)
 	c:EnableReviveLimit()
+	--banish until end phase
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(90000663,0))
+	e1:SetCategory(CATEGORY_NEGATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,0x1e0)
+	e1:SetTarget(c90000663.thtg)
+	e1:SetOperation(c90000663.thop)
+	c:RegisterEffect(e1)
 	--float and stuff
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetCode(EVENT_LEAVE_FIELD)
-	e2:SetCondition(c90000661.spcon)
-	e2:SetTarget(c90000661.sptg)
-	e2:SetOperation(c90000661.spop)
+	e2:SetCondition(c90000663.spcon)
+	e2:SetTarget(c90000663.sptg)
+	e2:SetOperation(c90000663.spop)
 	c:RegisterEffect(e2)
 end
 --condition
@@ -20,24 +33,56 @@ function c90000663.lcheck(g,lc)
 end
 
 --float stuff
-function c90000661.spcon(e,tp,eg,ep,ev,re,r,rp)
+function c90000663.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsPreviousPosition(POS_FACEUP)
 end
 
-function c90000661.spfilter(c,e,tp)
+function c90000663.spfilter(c,e,tp)
 	return (c:IsLevelBelow(6) and c:IsRace(RACE_BEAST)) or (c:IsLevelBelow(6) and c:IsAttribute(ATTRIBUTE_WIND)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
-function c90000661.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c90000663.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c90000661.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(c90000663.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
 end
-function c90000661.spop(e,tp,eg,ep,ev,re,r,rp)
+function c90000663.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c90000661.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c90000663.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+--negate until end phase
+function c90000663.thfilter(c)
+	return c:IsFaceup()
+end
+function c90000663.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local ct=c:GetLinkedGroupCount()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c90000663.thfilter(chkc) end
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(c90000663.thfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	local g=Duel.SelectTarget(tp,c90000663.thfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
+	c:RegisterFlagEffect(0,RESET_EVENT+0x1fe0000,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(90000663,1))
+end
+function c90000663.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if Duel.Negateg,0,REASON_EFFECT+REASON_TEMPORARY)~=0
+		local fid=c:GetFieldID()
+		local og=Duel.GetOperatedGroup()
+		for oc in aux.Next(og) do
+			oc:RegisterFlagEffect(90000663,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1,fid)
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e1:SetTarget(c90000663.filter)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_PHASE+PHASE_STANDBY+RESET_SELF_TURN)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
